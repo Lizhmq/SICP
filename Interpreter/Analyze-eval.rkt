6539ed118f07bf1ce1d44d70ccdd6b4ac8e71629
Lizhmq <1600012911@pku.edu.cn>
2018-05-26 18:29:01 +0800

    add interpreters

---
+++ Interpreter/Analyze-eval.rkt
@@ -0,0 +1,416 @@
+#lang racket
+
+;require r5rs用于实现mcons和set!
+(require r5rs)
+
+
+;my-eval定义
+;(define (my-eval exp env)
+;  (cond ((self-evaluating? exp) exp)                            ;自求值表达式
+;        ((variable? exp) (lookup-variable-value exp env))       ;变量
+;        ((and? exp) (eval-and (operands exp) env))
+;        ((or? exp) (eval-or (operands exp) env))                
+;        ((let? exp) (eval-let exp env))                   ;let表达式
+;        ((quoted? exp) (text-of-quotation exp))                 ;单引号表达式(符号)
+;        ((assignment? exp) (eval-assignment exp env))           ;赋值set!
+;        ((definition? exp) (eval-definition exp env))           ;定义define
+;        ((if? exp) (eval-if exp env))                           ;if表达式
+;        ((lambda? exp)
+;         (make-procedure (lambda-parameters exp)
+;                         (lambda-body exp) env))
+;        ((begin? exp)
+;         (eval-sequence (begin-actions exp) env))
+;        ((cond? exp) (my-eval (cond->if exp) env))              ;条件表达式
+;        ((application? exp)                                     ;上述都不成立时,视为过程调用
+;         (my-apply (my-eval (operator exp) env) (list-of-values (operands exp) env)))
+;        (else
+;         (error "Unknown expression type -- EVAL" exp))))
+
+;分析和执行分离
+
+(define (my-eval exp env)
+  ((analyze exp) env))
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+         (analyze-self-evaluating exp))
+        ((quoted? exp)
+         (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env) qval)))
+(define (analyze-variable exp)
+  (lambda (env) (lookup-variable-value exp env)))
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env)
+      (set-variable-value! var (vproc env) env))))
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env)
+      (define-variable! var (vproc env) env))))
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env)
+      (if (true? (pproc env))
+          (cproc env)
+          (aproc env)))))
+(define (analyze-lambda exp)
+  (let ((vars (lambda-parameters exp))
+        (bproc (analyze-sequence (lambda-body exp))))
+    (lambda (env) (make-procedure vars bproc env))))
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (my-apply (fproc env)
+                           (map (lambda (aproc) (aproc env))
+                                aprocs)))))
+(define (my-apply proc args)
+  (cond ((primitive-procedure? proc)
+         (apply (primitive-implementation proc) args))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))))
+        (else
+         (error
+          "Unknown procedure type -- MY-APPLY" proc))))
+
+
+
+;表达式类型判断,表达式部分分割
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (and? exp) (tagged-list? exp 'and))
+(define (or? exp) (tagged-list? exp 'or))
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp)                ;单引号开头的表达式会被转换成(quote ...)的list
+  (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (cadr exp))
+
+(define (assignment? exp)            ;(set! x y)
+  (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+(define (definition? exp)            ;define有两种形式 (define <var> <value>)
+  (tagged-list? exp 'define))                        ;(define (<var> paras) (body))
+(define (definition-variable exp)            ;后者等价于(define <var> (lambda (paras) (body)))
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)           ;paras
+                   (cddr exp))))         ;body
+
+(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)                 ;alternative部分可能不存在
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+(define (make-begin seq) (cons 'begin seq))
+
+(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond->if exp)                      
+  (expand-clauses (cond-clauses exp)))          
+(define (special-cond? clause)               ;特殊形式cond ([test] => [recipient])的实现
+  (and (pair? (cdr clause)) (eq? (cadr clause) '=>)))
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      (void)
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last -- COND->IF"
+                       clauses))
+            (make-if (cond-predicate first)
+                     (if (special-cond? first)
+                         (list (caddr first) (car first))
+                         (sequence->exp
+                          (if (null? (cond-actions first))
+                              (list (cond-predicate first))        ;cond-actions部分不存在时返回cond-predicate部分
+                              (cond-actions first))))              ;以actions的形式返回,故加上list
+                     (expand-clauses rest))))))
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+(define (list-of-values lst env)
+  (map (lambda (x) (my-eval x env)) lst))
+
+(define (let? exp) (tagged-list? exp 'let))
+(define (let-clause exp) (cadr exp))
+(define (let-body exp) (cddr exp))
+
+
+
+
+
+;表达式求值
+(define (eval-and ops env)          ;and表达式,如果ops都为真就返回最后一个值
+  (if (null? ops)
+      (error "eval-and error")
+      (if (null? (cdr ops))
+          (my-eval (car ops) env)
+          (if (my-eval (car ops) env)
+              (eval-and (cdr ops) env)
+              false))))
+(define (eval-or ops env)           ;or表达式,返回第一个不false的值
+  (if (null? ops)
+      false
+      (let ((cur (my-eval (car ops) env)))
+        (if cur
+            cur
+            (eval-or (cdr ops) env)))))
+
+(define (eval-let exp env)                  ;转换为lambda表达式,参数递归my-eval求值
+  (let* ((vals (map (lambda (x) (my-eval x env)) (map cadr (let-clause exp))))
+         (func (make-lambda
+                (map (lambda (x) (car x)) (let-clause exp))   ;paras
+                (let-body exp))))                             ;body
+    (my-apply (my-eval func env) vals)))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (my-eval (assignment-value exp) env)
+                       env))
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (my-eval (definition-value exp) env)
+    env))
+
+(define (true? x)
+  (not (eq? false x)))
+(define (eval-if exp env)
+  (if (true? (my-eval (if-predicate exp) env))
+      (my-eval (if-consequent exp) env)
+      (my-eval (if-alternative exp) env)))
+
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (my-eval (first-exp exps) env))
+        (else (my-eval (first-exp exps) env) (eval-sequence (rest-exps exps) env))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)              ;在env中寻找var的约束
+    (define (scan vars vals)
+      (cond ((null? vars)             ;env的vars列表中没有找到var
+             (env-loop (enclosing-environment env)))   ;去外围环境找
+            ((eq? var (car vars))
+             (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)           ;最外层环境
+        (error "Unbound variable" var)            ;未约束变量
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+
+
+
+
+;环境
+(define (make-frame variables values)
+  (cons variables values))
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values frame))))
+;初始过程,用于构建初始环境
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cadr cadr)
+        (list 'list list)
+        (list 'cons cons)
+        (list 'pair? pair?)
+        (list 'append append)
+        (list 'caddr caddr)
+        (list '+ +)
+        (list '- -)
+        (list '* *)
+        (list '/ /)
+        (list '< <)
+        (list '> >)
+        (list '= =)
+        (list 'assoc assoc)
+        (list 'length length)
+        (list 'not not)
+        (list 'eq? eq?)
+        (list 'remainder remainder)
+        (list 'sqrt sqrt)
+        (list 'symbol? symbol?)
+        (list 'number? number?)
+        (list 'null? null?)))
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))     ;初始过程用'primitive标签
+       primitive-procedures))
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define glb-env (setup-environment))
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+(define (primitive-implementation proc) (cadr proc))
+;(define (my-apply procedure arguments)                 ;过程apply时在新的内层环境中求值
+;  (cond ((primitive-procedure? procedure)
+;         (apply (primitive-implementation procedure) arguments))
+;        ((compound-procedure? procedure)
+;         (eval-sequence
+;          (procedure-body procedure)
+;          (extend-environment
+;           (procedure-parameters procedure)
+;           arguments
+;           (procedure-environment procedure))))
+;        (else
+;         (error "unkonwn procedure type -- APPLY" procedure))))
+
+
+
+
+;输入输出
+(define input-prompt ">>> ")
+(define output-prompt "")
+(define (driver-loop)
+  (let ((input (read)))
+    (if (eq? input eof)
+        (void)
+        (begin
+          (let ((output (my-eval input glb-env)))
+            (if (not (eq? output (void)))
+                (user-print output)))
+          (driver-loop)))))
+(define (prompt-for-input string)
+  (display string))
+(define (announce-output string)
+  (display string))
+(define (user-print object)
+  (if (compound-procedure? object)
+      (displayln "#<procedure>")
+      (displayln object)))
+;将大括号转换为小括号
+(print-mpair-curly-braces #f)
+(driver-loop)
--- Interpreter/readme.md
+++ Interpreter/readme.md
@@ -1,42 +1,40 @@
-# Scheme解释器
+#Scheme解释器
 
-元循环：用一种语言实现自身的求值器
-
-用Scheme实现Scheme求值器，以Scheme程序作为输入，输出为运行结果
-
-求值环境模型在[gw_sicp](https://github.com/Lizhmq/SICP/blob/master/Texts/gw_sicp_03_01.pdf)有详细描述
+* 元循环：用一种语言实现自身的求值器
+* 用Scheme实现Scheme求值器，以Scheme程序作为输入，输出为运行结果
+* 求值环境模型在[gw_sicp_1](https://github.com/Lizhmq/SICP/blob/master/Texts/gw_sicp_03_01.pdf)有详细描述
 
 
 
-## my-eval函数
+##my-eval函数
 
-*基本表达式
+* 基本表达式
 
-​	自求值表达式（数、字符串）：直接返回
+		自求值表达式（数、字符串）：直接返回
 
-​	变量：从环境env中读出当前的值
+		变量：从环境env中读出当前的值
 
-*特殊形式
+* 特殊形式
 
-​	单引号表达式（如'(1 2 3)）：返回单引号后面的表达式
+		单引号表达式（如'(1 2 3)）：返回单引号后面的表达式
 
-​	变量赋值或定义：递归调用my-eval计算所赋的值，然后在环境中修改变量的值或增加约束
+		变量赋值或定义：递归调用my-eval计算所赋的值，然后在环境中修改变量的值或增加约束
+	
+		if表达式：根据条件部分的求值结果求值相应子表达式
+	
+		lambda表达式：建立过程对象，在环境中做相应修改
+	
+		begin表达式：按顺序求值各个表达式
+	
+		cond表达式：变换为嵌套的if表达式
+	
+		let表达式：转换为lambda表达式，参数通过my-eval递归求值得到，然后运用my-apply
+	
+		增加了and,or的实现，需要注意的是，scheme中的and、or不仅是返回true/false，当and表达式为真时，返回最后一个子表达式的值，or表达式返回第一个不是false的值
 
-​	if表达式：根据条件部分的求值结果求值相应子表达式
+* 组合式（过程）
 
-​	lambda表达式：建立过程对象，在环境中做相应修改
-
-​	begin表达式：按顺序求值各个表达式
-
-​	cond表达式：变换为嵌套的if表达式
-
-​	let表达式：转换为lambda表达式，参数通过my-eval递归求值得到，然后运用my-apply
-
-​	增加了and,or的实现，需要注意的是，scheme中的and、or不仅是返回true/false，当and表达式为真时，返回最后一个子表达式的值，or表达式返回第一个不是false的值
-
-*组合式（过程）
-
-​	递归地求值运算符部分和运算对象部分，将得到的过程和参数应用于my-apply
+		递归地求值运算符部分和运算对象部分，将得到的过程和参数应用于my-apply
 
 
 
@@ -62,9 +60,8 @@
 
 成分切割大部分比较使用简单的car、cdr组合就可以完成，下面提几个特殊的情况：
 
-*if表达式分为predicate、consequent和alternative三部分，需要判断alternative部分是否存在
-
-*cond增加特殊形式:
+* if表达式分为predicate、consequent和alternative三部分，需要判断alternative部分是否存在
+* cond增加特殊形式:
 
 ```scheme
 cond ([test] => [recipient])
@@ -74,7 +71,7 @@ cond ([test] => [recipient])
 
 cond-actions部分不存在时，以cond-predicate部分为返回值
 
-*define表达式有两种形式：
+* define表达式有两种形式：
 
 ```scheme
 (define <var> <value>)
@@ -87,13 +84,10 @@ cond-actions部分不存在时，以cond-predicate部分为返回值
 
 ## 对应类型的求值
 
-*eval-and、eval-or、eval-if、eval-sequence较简单
-
-*lambda表达式my-eval后得到环境中约束的过程
-
-*eval-let转换为lambda表达式后，my-eval得到过程再用my-apply求值
-
-*eval-assignment、eval-definition和lookup-variable-value的对应与环境框架有关
+* eval-and、eval-or、eval-if、eval-sequence较简单
+* lambda表达式my-eval后得到环境中约束的过程
+* eval-let转换为lambda表达式后，my-eval得到过程再用my-apply求值
+* eval-assignment、eval-definition和lookup-variable-value的对应与环境框架有关
 
 
 
@@ -103,8 +97,78 @@ cond-actions部分不存在时，以cond-predicate部分为返回值
 
 frame也是一个序对，car部分是约束的变量组成的list，cdr部分是对应的值的list
 
-![environment](https://github.com/Lizhmq/SICP/blob/master/Interpreter/environment.PNG)
+![environment](C:\Users\Joe\Documents\GitHub\SICP\Interpreter\Pics\environment.PNG)
 
 求值过程时，建立新的内层环境并在其中求值
 
-![env](https://github.com/Lizhmq/SICP/blob/master/Interpreter/env.PNG)
+![env](C:\Users\Joe\Documents\GitHub\SICP\Interpreter\Pics\env.PNG)
+
+
+
+
+
+
+
+
+
+# 正则序解释器
+
+* 一般来说，Scheme采用的时应用序求值，即在调用过程之前便对所有参数求值
+* 惰性求值：将参数求值推迟到实际需要时
+
+```scheme
+;在这个例子中，正则序可以运行，而应用序报错
+(define (try a b)
+  (if (= a 0) 1 b))
+(try 0 (/ 1 0))
+```
+
+* 惰性求值的实现
+  * 为参数建立一个称为槽(thunk)的特殊对象，当需要使用时，从槽中取出参数（称为强迫）
+  * 需要强迫槽的三种情况：
+    * 某个基本过程要使用槽的值
+    * 槽的值被作为条件表达式的谓词
+    * 某个要求值的表达式以槽为运算符
+  * 对应这三种情况，分别修改对应的过程（如eval-if、my-apply等）
+  * 可以实现记忆性的槽，一定程度上提高效率（需要考虑可能对语义造成的影响）
+* 用过程定义cons、car、cdr，由于参数的惰性求值，列表便自然地成为流
+  * 需要注意的是输入中形如'(a b c)的列表要人为地转化为定义的cons对，否则无法运用car、cdr方法
+
+
+
+
+
+# 可指定参数是否严格的解释器
+
+* 上面对解释器的修改有一个严重的问题--不是前向兼容的
+* 因此，可以实现能指定参数是否严格的解释器从而在兼容的前提下同时实现惰性求值
+* 参数的严格性：
+  * 如果过程要求在执行过程体之前对某个参数求值，则称该过程对该参数是严格的，否则是不严格的
+  * 纯应用序的语言过程对所有参数都是严格的
+  * 纯正则序的语言，复合过程对所有参数都不严格，基本过程可以是严格的，也可以是不严格的
+  * 允许某些过程的某些参数不严格有时很方便，例如用cons构造数据结构时可以在结构尚未完全确定时就使用它（例如流的例子）
+* 主要修改my-apply中的参数解析
+
+
+
+
+
+
+
+
+
+# 分析和执行分离的解释器
+
+* 在前面实现的Scheme求值器中，以procedure形式实现的过程中包含源代码：
+
+  ```scheme
+  (procedure (n)
+             ((if (= n 1)
+                  1
+                  (* (factorial (- n 1) n)))
+              glb-env)
+  ```
+
+  每次递归调用factorial，都需要在环境中寻找对象的约束，然后对函数体源代码分析和求值，十分浪费。
+
+* 实现语法分析与求值分离，先对表达式analyze得到一个参数为env的过程，然后eval求值。
